2e80bbd2b9d4c30f8118424cc5332c13df817c7b
Joseph Lee <joseph@idealist.org>
2015-01-16 17:47:52 -0800

    Import tweak, basic error message

--- simplio/simplio.py
+++ simplio/simplio.py
@@ -1,4 +1,4 @@
-from sys import argv, stdin, stdout
+import sys
 
 
 def simplio(func):
@@ -8,22 +8,25 @@ def simplio(func):
     Determines if input and output are command-line argument file names or
     STDIN and STDOUT, or mix of one of each.
     """
-    argc = len(argv)
+    argc = len(sys.argv)
+
     if argc == 3:
-        infile = open(argv[1], 'r')
-        outfile = open(argv[2], 'w')
+        infile = open(sys.argv[1], 'r')
+        outfile = open(sys.argv[2], 'w')
     elif argc == 2:
-        if not stdin.isatty():
-            infile = stdin
-            outfile = open(argv[1], 'w')
+        if not sys.stdin.isatty():
+            infile = sys.stdin
+            outfile = open(sys.argv[1], 'w')
         else:
-            infile = open(argv[1], 'r')
-            outfile = stdout
-    elif argc == 1 and not stdin.isatty():
-        infile = stdin
-        outfile = stdout
+            infile = open(sys.argv[1], 'r')
+            outfile = sys.stdout
+    elif argc == 1 and not sys.stdin.isatty():
+        infile = sys.stdin
+        outfile = sys.stdout
     else:
-        raise IOError()
+        # TODO: more specific error handling
+        raise IOError('invalid input')
+
     def wrapper():
         rval = func(infile, outfile)
         if not infile.closed:
@@ -31,4 +34,5 @@ def simplio(func):
         if not outfile.closed:
             outfile.close()
         return rval
+
     return wrapper
